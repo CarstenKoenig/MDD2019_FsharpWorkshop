552aa25ead9f9f233fbbcfad97a15806899d3fe0
Carsten König <carsten@gettingsharper.de>
2019-05-18 19:10:34 +0200

    Sudoku Projekt nach .net core

---
+++ Projekt/src/Sudoku.Tests/ListenTests.fs
@@ -0,0 +1,55 @@
+﻿namespace Sudoku.Tests
+
+open Sudoku
+open Xunit
+open Hedgehog
+
+
+module ``group und ungroup`` =
+    open Matrix
+
+    [<Fact>]
+    let ``alle Elemente von group 3 haben Länge 3`` () =
+        property {
+            let! xs = Gen.list (Range.linear 0 10) (Gen.int (Range.linear 1 50))
+            // stelle sicher, dass xs genügend Elemente hat
+            let xs = xs @ xs @ xs
+            return 
+                group 3 xs
+                |> List.forall (fun l -> List.length l = 3)
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``group 3 >> ungroup = id (für gültige Eingaben)`` () =
+        property {
+            let! xs = Gen.list (Range.linear 0 10) (Gen.int (Range.linear 1 50))
+            // stelle sicher, dass xs genügend Elemente hat
+            let xs = xs @ xs @ xs
+            return ungroup (group 3 xs) = xs
+        }
+        |> Property.check
+        
+
+module ``crossProd`` =
+
+    [<Fact>]
+    let ``crossProd einer Liste von singletons ist enthält nur diese``() =
+        property {
+            let! xs = Gen.list (Range.linear 0 10) (Gen.int (Range.linear 1 50))
+            let xss = List.map Seq.singleton xs
+            return List.ofSeq (crossProd xss) = [xs]
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``|crossProd xss| = Produkt der Längen der xs in xss``() =
+        property {
+            let! xs = Gen.list (Range.linear 0 10) (Gen.int (Range.linear 1 50))
+            let! ys = Gen.list (Range.linear 0 10) (Gen.int (Range.linear 1 50))
+            let xss = [xs; ys]
+            return Seq.length (crossProd xss) = xs.Length * ys.Length
+        }
+        |> Property.check
---
+++ Projekt/src/Sudoku.Tests/MatrixTests.fs
@@ -0,0 +1,84 @@
+﻿namespace Sudoku.Tests
+
+open Sudoku
+open Xunit
+open Hedgehog
+
+open Sudoku.Game
+
+[<AutoOpenAttribute>]
+module private Generators = 
+    let zifferGen =
+        Gen.int (Range.linear 1 9)
+
+    let zelleGen =
+        Gen.choice
+            [
+                Gen.constant Leer
+                Gen.map Zahl zifferGen
+            ]
+
+    let eingabeGen : Gen<Eingabe> =
+        let reiheGen = Gen.list (Range.singleton 9) zelleGen
+        Gen.list (Range.singleton 9) reiheGen
+
+
+module ``Matrix Involutionen für Sudoku Eingaben`` =
+    open Matrix
+
+    [<Fact>]
+    let ``transponiere >> transponiere = id`` () =
+        property {
+            let! g = eingabeGen
+            return transponiere (transponiere g) = g
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``transponiere für Beispiel ist korrekt`` () =
+        transponiere [[1;2];[3;4]] = [[1;3];[2;4]]
+
+
+    [<Fact>]
+    let ``Zeilen >> Zeilen = id`` () =
+        property {
+            let! g = eingabeGen
+            return zeilen (zeilen g) = g
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``zeilen für Beispiel ist korrekt`` () =
+        zeilen [[1;2];[3;4]] = [[1;2];[3;4]]
+
+
+    [<Fact>]
+    let ``Spalten >> Spalten = id`` () =
+        property {
+            let! g = eingabeGen
+            return spalten (spalten g) = g
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``spalten für Beispiel ist korrekt`` () =
+        spalten [[1;2];[3;4]] = [[1;3];[2;4]]
+
+
+    [<Fact>]
+    let ``Blöcke >> Blöcke = id`` () =
+        property {
+            let! g = eingabeGen
+            return bloecke 3 (bloecke 3 g) = g
+        }
+        |> Property.check
+
+
+    [<Fact>]
+    let ``bloecke für Beispiel ist korrekt`` () =
+        bloecke 2 [[1;2;3;4];[5;6;7;8];[9;10;11;12];[13;14;15;16]] = 
+            [[1;2;5;6];[3;4;7;8];[9;10;13;14];[11;12;15;16]]
+
